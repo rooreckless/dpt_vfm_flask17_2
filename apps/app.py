852dca1a052700d8a02f79e8073ba12f802033b5
rooreckless <rooreckless@gmail.com>
2023-07-18 22:30:57 +0900

    02 testing nvf

--- apps/app.py
+++ apps/app.py
@@ -1,7 +1,7 @@
 from apps.config import config
 from flask import Flask
 from apps.testbp import views
-
+from flask_cors import CORS
 def create_app(config_key):
     app=Flask(__name__)
     app.register_blueprint(views.testbp,url_prefix="/testbp")
@@ -9,6 +9,9 @@ def create_app(config_key):
     #確認用にapp.configを出力してみます
     print("----app.config[SECRET_KEY]=",app.config['SECRET_KEY'])
     print("----app.config[SQLALCHEMY_ECHO]=",app.config['SQLALCHEMY_ECHO'])
+    # CORS(app, resources={"/testbp/*":{"origins":"http://localhost:5173"}})
+    CORS(app, resources={"/testbp/*":{"origins":"http://tmp/gunicorn_flask.sock"}})
+    
     return app
 
 if __name__=="__main__":
--- apps/testbp/views.py
+++ apps/testbp/views.py
@@ -1,10 +1,12 @@
-from flask import Flask,Blueprint,render_template,redirect,flash,url_for,request
+from flask import Flask,Blueprint,render_template,redirect,flash,url_for,request,jsonify
 #ブループリントを作成
 # url_prefixを追加したBlueprintオブジェクトを作成
 testbp = Blueprint("testbp",__name__,url_prefix="/testbp",template_folder='templates',static_folder='static')
 
-#auth側のルーティングを記載
-@testbp.route("/")
-def index():
-    #apps/testbp/templates の中の、testbp/index.htmlを指定
-    return render_template("testbp/index.html")
+# @testbp.route("/")
+# def index():
+#     return render_template("testbp/index.html")
+#テスト用のルーティングです
+@testbp.route("/testroute",methods=["GET"],endpoint="testfectchapi")
+def testfectchapi():
+  return jsonify({"flask-- @back_app.route(/test":"is finished"})
