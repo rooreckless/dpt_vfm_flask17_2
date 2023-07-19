aa05e5ddd9c2dc2ae412dda127859c9616c197be
rooreckless <rooreckless@gmail.com>
2023-07-20 07:54:32 +0900

    02 testing nvf

--- apps/testbp/views.py
+++ apps/testbp/views.py
@@ -1,12 +1,17 @@
-from flask import Flask,Blueprint,render_template,redirect,flash,url_for,request,jsonify
+from flask import Flask,Blueprint,jsonify
+import datetime
 #ブループリントを作成
 # url_prefixを追加したBlueprintオブジェクトを作成
 testbp = Blueprint("testbp",__name__,url_prefix="/testbp",template_folder='templates',static_folder='static')
 
+#以下はflaskで描画するためのルートなので潰します
 # @testbp.route("/")
 # def index():
 #     return render_template("testbp/index.html")
-#テスト用のルーティングです
+
+#vueからのアクセステスト用のルートです
 @testbp.route("/testroute",methods=["GET"],endpoint="testfectchapi")
 def testfectchapi():
-  return jsonify({"flask-- @back_app.route(/test":"is finished"})
+  dt_now = datetime.datetime.now()
+  return jsonify({"flask-- @testbp.route(/testroute":"is finished",
+                  "nowtime":dt_now.strftime('%Y年%m月%d日 %H:%M:%S')})
